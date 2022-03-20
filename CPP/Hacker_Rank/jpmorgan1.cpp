c0e97251c575532491b19e9fd7bb0ef6920a52a5
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2022-03-20 10:57:08 +0530

    commented

--- CPP/Hacker_Rank/jpmorgan1.cpp
+++ CPP/Hacker_Rank/jpmorgan1.cpp
@@ -2,19 +2,19 @@
 #include <vector>
 using namespace std;
 
-int ans()
-{
-    int addCardSum = 0, x = 0;
-    for (auto i = cardTypes.begin(); i != cardTypes.end(); i++)
-    {
-        if (*i % 2 != 0)
-            addCardSum++;
-    }
-    for (auto i = cardTypes.begin(); i != cardTypes.end(); i++)
-    {
-        if (*i % 3 != 0)
-            x++;
-    }
-    return addCardSum < x ? addCardSum : x;
-};
+// int ans()
+// {
+//     int addCardSum = 0, x = 0;
+//     for (auto i = cardTypes.begin(); i != cardTypes.end(); i++)
+//     {
+//         if (*i % 2 != 0)
+//             addCardSum++;
+//     }
+//     for (auto i = cardTypes.begin(); i != cardTypes.end(); i++)
+//     {
+//         if (*i % 3 != 0)
+//             x++;
+//     }
+//     return addCardSum < x ? addCardSum : x;
+// };
 int main() {}
